65e1e28e45a1d5c39074c90e1cea2f2f47c7744c
tristan499 <Tristancalliste499@gmail.com>
2026-03-07 21:54:36 -0400

    Completed voting and custom background

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,6 +19,7 @@ from django.urls import path
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('polls/', include('polls.urls')),
+    path('admin/', admin.site.urls),
+   
 ]
